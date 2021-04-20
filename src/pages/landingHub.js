96572fa5e7f24e6a954934e843faaed5a1c191c5
William Wassmann <wmwassmann@hotmail.com>
2021-04-19 19:23:45 -0700

    pushing small changes

--- src/pages/landingHub.js
+++ src/pages/landingHub.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Typography } from '@material-ui/core';
-
 import Typed from "react-typed";
+import './css/style.css'
 
 export default function landingHub() {
 
@@ -10,9 +10,9 @@ export default function landingHub() {
     
     return (
         
-        <div>
-            <Typography variant="h4">
-              <Typed strings={["William Wassmann"]} typeSpeed={40} />
+        <div className='typed-intro'>
+            <Typography variant="h2">
+              <Typed strings={["Hello, and welcome to my playground!"]} typeSpeed={40} />
           </Typography>
         </div>
     )

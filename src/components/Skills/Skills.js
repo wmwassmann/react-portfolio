9a5d6d5cdfb0a8d958f53f630b6f9d0da6dc2286
William Wassmann <wmwassmann@hotmail.com>
2021-08-11 10:58:45 -0700

    adding more to the skills buttons. Practicing quality and maintainable code

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,30 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './css/skills-style.css'
 
 const Skills = () => {
 
-   
+    const[skill, setSkill] = useState({ currentSkill: '' })
 
-    const handle_skills = () => {
-      
-  
+    const handle_skills = (e) => {
+        setSkill(e.target.name)
     }
 
+    useEffect(() => {
+        console.log(skill)
+    
+    }, [skill])
 
 
 
     return (
         <div className='button-container'>
-            <button className='skills-button' id='languages' onClick={handle_skills}>       
+            <button className='skills-button' id='languages' name='languages' onClick={handle_skills}>       
                 Languages         
             </button>
-            <button className='skills-button' id='framework' onClick={handle_skills}>                
+            <button className='skills-button' id='framework' name='framework' onClick={handle_skills}>                
                 Frameworks
             </button>
-            <button className='skills-button' id='databases' onClick={handle_skills}>      
+            <button className='skills-button' id='databases' name='databases' onClick={handle_skills}>      
                 Databases          
             </button>
-            <button className='skills-button' id='dev-tools' onClick={handle_skills}>     
+            <button className='skills-button' id='dev-tools' name='dev-tools' onClick={handle_skills}>     
                 Development Tools           
             </button>
             
